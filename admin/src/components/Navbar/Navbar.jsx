3f508715683a8df93948feff8eacc91c92ed2243
Sunil-Patel-21 <patelsunil60273@gmail.com>
2026-02-27 10:14:05 +0530

    name added for website

--- admin/src/components/Navbar/Navbar.jsx
+++ admin/src/components/Navbar/Navbar.jsx
@@ -5,7 +5,7 @@ import { assets } from "../../assets/assets";
 const Navbar = () => {
   return (
     <div className="navbar">
-      <img className="logo" src={assets.logoo} alt="" />
+      <h1 className="logo">Gift Cart System</h1>
       <img className="profile" src={assets.profile_image} alt="" />
     </div>
   );
